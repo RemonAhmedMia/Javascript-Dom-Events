c37dfcfac8c619fbbdc375e58c8bb392129a5b4a
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-18 20:30:24 +0100

    final-one.event-01

---
+++ js/final-event.js
@@ -0,0 +1,6 @@
+function handleChangeText (){
+    const changeText = document.getElementById("change-heading");
+    changeText.innerText = 'Hello Programming Hero Piopol';
+    changeText.style.color = 'red';
+    changeText.style.fontSize = '50px'
+}
