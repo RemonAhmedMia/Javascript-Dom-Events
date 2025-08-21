8c45864ba96ee529f0cb243015b35431bc05a0bb
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-21 21:13:31 +0100

    add-dlt-button

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// type-01:
 
+document.getElementById("hide-input").addEventListener('keyup', function(event){
+    const text = event.target.value;
+    const deleteButton = document.getElementById("dlt-btn");
+    if(text === "delete") {
+        deleteButton.removeAttribute("disabled")
+    } else{
+        deleteButton.setAttribute("disabled" , true)
+    }
+})
+
+document.getElementById("dlt-btn").addEventListener('click' , function(){
+    const hidetext = document.getElementById("hide-text");
+    hidetext.style.display = "none"
+})
