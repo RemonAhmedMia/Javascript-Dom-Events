5a90173bb0858c38ff53cc9aea73fb13b24ac91c
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-21 10:33:31 +0100

    event-type-02/input-click

--- js/event-summury.js
+++ js/event-summury.js
@@ -4,5 +4,26 @@ function handleOnclick() {
 
 function handleOnclick() {
     const changeText = document.getElementById("change-heading");
-    changeText.innerText = "Creative It Onek vlo"
+    changeText.innerText = "Creative It Onek vlo";
+    changeText.style.color = "green";
 }
+
+// =====================Type-02
+
+document.getElementById("event-click").addEventListener('click', function(){
+    const changeText = document.getElementById("change-heading");
+    changeText.innerText = "My Name Is Sania Ahmed Im a Student Of Creative It";
+    changeText.style.fontSize = "50px";
+    changeText.style.color = "red";
+    changeText.style.textTransform = "uppercase";
+})
+
+// =======================Tyep-03
+
+document.getElementById("btn-update").addEventListener('click' , function(){
+    const inputFeild = document.getElementById("input-feild");
+    const inputText = inputFeild.value ;
+    const paragraph = document.getElementById("update-paragraph");
+    paragraph.innerText = inputText ;
+    inputFeild.value = " " ;
+})
